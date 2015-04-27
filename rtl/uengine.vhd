0206d155f437b949a37a61cae8f2f20e885bde72
lemoniac <aldomon@gmail.com>
2015-04-28 00:06:28 +0100

    Added jmp absolute to uops

--- rtl/uengine.vhd
+++ rtl/uengine.vhd
@@ -45,11 +45,15 @@ architecture syn of UENGINE is
 	constant One : unsigned(15 downto 0) := "0000000000000001";
 	constant MaxInt : unsigned(15 downto 0) := "1111111111111111";
 
+	constant IncPc : unsigned(1 downto 0) := "00";
+	constant JmpRel : unsigned(1 downto 0) := "01";
+	constant JmpAbs : unsigned(1 downto 0) := "10";
+
 	subtype reg_op_t is integer range 0 to 15;
 
 	type ucode_t is
 		record
-			inc_pc : std_logic;
+			pc : unsigned(1 downto 0);
 			alu_op : alu_op_t;
 			src_0 : reg_op_t;
 			src_1 : reg_op_t;
@@ -61,12 +65,13 @@ architecture syn of UENGINE is
 	type ucode_array_t is array (0 to 15) of ucode_t;
 	
 	constant ucode : ucode_array_t := (
-		('1', AluNone, 0, 0, 0, '0', Zero),
-		('1', AluCopy, 0, 0, 1, '1', One), -- R1 <- 1
-		('1', AluCopy, 0, 0, 2, '1', One), -- R2 <- 1
-		('1', AluAdd, 1, 2, 0, '0', Zero), -- R0 <- R1 + R2
-		('1', AluCopy, 0, 0, 0, '1', One), -- R0 <- 1
-		others => ('0', AluNone, 0, 0, 0, '0', Zero)
+		(IncPc, AluCopy, 0, 0, 0, '1', Zero),  -- R0 <- 0
+		(IncPc, AluCopy, 0, 0, 1, '1', One),   -- R1 <- 1
+		(IncPc, AluCopy, 0, 0, 2, '1', One),   -- R2 <- 1
+		(IncPc, AluAdd,  1, 2, 0, '0', Zero),  -- R0 <- R1 + R2
+		(IncPc, AluCopy, 0, 0, 0, '1', One),   -- R0 <- 1
+		(JmpAbs, AluNone, 0, 0, 0, '0', Zero), -- JMP 0
+		others => (JmpAbs, AluNone, 0, 0, 0, '0', Zero)
 		);
 
 begin
@@ -76,10 +81,7 @@ begin
 		if rising_edge(clkin) then
 			if count = 0 then
 				uop := ucode(uPC);
-				if uop.inc_pc = '1' then
-					uPC <= uPC + 1;
-				end if;
-				
+
 				if uop.alu_op = AluCopy then
 					if uop.use_immediate = '1' then
 						R(uop.dst) <= uop.immediate;
@@ -87,6 +89,13 @@ begin
 				elsif uop.alu_op = AluAdd then
 					R(uop.dst) <= R(uop.src_0) + R(uop.src_1);
 				end if;
+
+				if uop.pc = IncPc then
+					uPC <= uPC + 1;
+				elsif uop.pc = JmpAbs then
+					uPC <= to_integer(uop.immediate);
+				end if;
+
 				
 				LED1 <= R(0)(0);
 				LED2 <= R(0)(1);
