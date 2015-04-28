33f7ce3f7b5732b4c76f3a57e9a71fa351017dbc
lemoniac <aldomon@gmail.com>
2015-04-29 00:51:43 +0100

    Added ALU logical ops.

--- rtl/uengine.vhd
+++ rtl/uengine.vhd
@@ -25,6 +25,8 @@ architecture syn of UENGINE is
 
 	signal flag_Z : std_logic; -- zero
 	signal flag_C : std_logic; -- carry
+	signal flag_N : std_logic; -- negative
+	signal flag_V : std_logic; -- overflooded
 
 	signal uPC : integer range 0 to 65535;
 	--signal ADDR: unsigned(15 downto 0) := (others => '0');
@@ -38,11 +40,18 @@ architecture syn of UENGINE is
 
 	signal Z : unsigned(15 downto 0) := (others => '0');
 
-	subtype alu_op_t is std_logic_vector(2 downto 0);
+	subtype alu_op_t is std_logic_vector(3 downto 0);
 	
-	constant AluNone : alu_op_t := "000";
-	constant AluCopy : alu_op_t := "001";
-	constant AluAdd : alu_op_t := "010";
+	constant AluNone : alu_op_t := "0000";
+	constant AluMove : alu_op_t := "0001";
+	constant AluAdd  : alu_op_t := "0010";
+	constant AluSub  : alu_op_t := "0011";
+	constant AluLsh  : alu_op_t := "0100";
+	constant AluRsh  : alu_op_t := "0101";
+	constant AluAnd  : alu_op_t := "0110";
+	constant AluOr   : alu_op_t := "0111";
+	constant AluXor  : alu_op_t := "1000";
+	constant AluNot  : alu_op_t := "1001";
 
 	constant Zero : unsigned(15 downto 0) := "0000000000000000";
 	constant One : unsigned(15 downto 0) := "0000000000000001";
@@ -77,34 +86,54 @@ architecture syn of UENGINE is
 	type ucode_array_t is array (0 to 15) of ucode_t;
 	
 	constant ucode : ucode_array_t := (
-		(IncPc, CondAlways, AluCopy, 0, 0, 0, '1', Zero),  -- R0 <- 0
-		(IncPc, CondAlways, AluCopy, 0, 0, 1, '1', One),   -- R1 <- 1
-		(IncPc, CondAlways, AluCopy, 0, 0, 2, '1', One),   -- R2 <- 1
+		(IncPc, CondAlways, AluMove, 0, 0, 0, '1', MaxInt),  -- R0 <- 11
+		(IncPc, CondAlways, AluNot, 0, 0, 0, '1', MaxInt),  -- R0 <- 0
+		(IncPc, CondAlways, AluMove, 0, 0, 1, '1', One),   -- R1 <- 1
+		(IncPc, CondAlways, AluMove, 0, 0, 2, '1', One),   -- R2 <- 1
 		(IncPc, CondAlways, AluAdd,  1, 2, 0, '0', Zero),  -- R0 <- R1 + R2
-		(IncPc, CondAlways, AluCopy, 0, 0, 0, '1', One),   -- R0 <- 1
-		(JmpAbs, CondNotZero, AluNone, 0, 0, 0, '0', One), -- JMP 1
-		(JmpAbs, CondZero, AluNone, 0, 0, 0, '0', Zero), -- JMP 0
+		(IncPc, CondAlways, AluMove, 0, 0, 0, '1', One),   -- R0 <- 1
+		(JmpAbs, CondNotZero, AluNone, 0, 0, 0, '0', Zero), -- JMP 0
+		(JmpAbs, CondZero, AluNone, 0, 0, 0, '0', One), -- JMP 1
 		others => (JmpAbs, CondAlways, AluNone, 0, 0, 0, '0', Zero)
 		);
 
 begin
 	process(clkin)
 		variable uop : ucode_t;
-		variable res : unsigned(16 downto 0);
+		variable res17 : unsigned(16 downto 0);
+		variable res : unsigned(15 downto 0);
+		variable B : unsigned(15 downto 0);		
 	begin
 		if rising_edge(clkin) then
 			if count = 0 then
 				uop := ucode(uPC);
 
-				if uop.alu_op = AluCopy then
+				if uop.alu_op /= AluNone then
 					if uop.use_immediate = '1' then
-						R(uop.dst) <= uop.immediate;
+						B := uop.immediate;
+					else
+						B := R(uop.src_1);
 					end if;
-				elsif uop.alu_op = AluAdd then
-					res := ("0" & R(uop.src_0)) + ("0" & R(uop.src_1));
-					R(uop.dst) <= res(15 downto 0);
-					flag_C <= res(16);
-					if res(15 downto 0) = Zero then
+
+					if uop.alu_op = AluMove then
+						res := uop.immediate;
+					elsif uop.alu_op = AluAdd then
+						res17 := ("0" & R(uop.src_0)) + ("0" & B);
+						res := res17(15 downto 0);
+						flag_C <= res17(16);
+					elsif uop.alu_op = AluAnd then
+						res := R(uop.src_0) and B;
+					elsif uop.alu_op = AluOr then
+						res := R(uop.src_0) or B;
+					elsif uop.alu_op = AluXor then
+						res := R(uop.src_0) xor B;
+					elsif uop.alu_op = AluNot then
+						res := not B;
+					end if;
+
+					R(uop.dst) <= res;
+
+					if res = Zero then
 						flag_Z <= '1';
 					else
 						flag_Z <= '0';
