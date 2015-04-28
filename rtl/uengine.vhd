9d544cbc55084363d19c369088ce9ed2d007ffad
lemoniac <aldomon@gmail.com>
2015-04-28 23:51:27 +0100

    Added zero and carry flags. Conditional jump

--- rtl/uengine.vhd
+++ rtl/uengine.vhd
@@ -23,6 +23,9 @@ architecture syn of UENGINE is
 	type registers_t is array (0 to 15) of register_t;
 	signal R : registers_t;
 
+	signal flag_Z : std_logic; -- zero
+	signal flag_C : std_logic; -- carry
+
 	signal uPC : integer range 0 to 65535;
 	--signal ADDR: unsigned(15 downto 0) := (others => '0');
 	signal ADDR: integer range 0 to 65535;
@@ -49,11 +52,20 @@ architecture syn of UENGINE is
 	constant JmpRel : unsigned(1 downto 0) := "01";
 	constant JmpAbs : unsigned(1 downto 0) := "10";
 
+	subtype cond_t is std_logic_vector(2 downto 0);
+	constant CondNever : cond_t := "000";
+	constant CondZero : cond_t := "001";
+	constant CondCarry : cond_t := "010";
+	constant CondNotZero : cond_t := "101";
+	constant CondNotCarry : cond_t := "110";
+	constant CondAlways : cond_t := "111";
+
 	subtype reg_op_t is integer range 0 to 15;
 
 	type ucode_t is
 		record
 			pc : unsigned(1 downto 0);
+			cond : cond_t;
 			alu_op : alu_op_t;
 			src_0 : reg_op_t;
 			src_1 : reg_op_t;
@@ -65,18 +77,20 @@ architecture syn of UENGINE is
 	type ucode_array_t is array (0 to 15) of ucode_t;
 	
 	constant ucode : ucode_array_t := (
-		(IncPc, AluCopy, 0, 0, 0, '1', Zero),  -- R0 <- 0
-		(IncPc, AluCopy, 0, 0, 1, '1', One),   -- R1 <- 1
-		(IncPc, AluCopy, 0, 0, 2, '1', One),   -- R2 <- 1
-		(IncPc, AluAdd,  1, 2, 0, '0', Zero),  -- R0 <- R1 + R2
-		(IncPc, AluCopy, 0, 0, 0, '1', One),   -- R0 <- 1
-		(JmpAbs, AluNone, 0, 0, 0, '0', Zero), -- JMP 0
-		others => (JmpAbs, AluNone, 0, 0, 0, '0', Zero)
+		(IncPc, CondAlways, AluCopy, 0, 0, 0, '1', Zero),  -- R0 <- 0
+		(IncPc, CondAlways, AluCopy, 0, 0, 1, '1', One),   -- R1 <- 1
+		(IncPc, CondAlways, AluCopy, 0, 0, 2, '1', One),   -- R2 <- 1
+		(IncPc, CondAlways, AluAdd,  1, 2, 0, '0', Zero),  -- R0 <- R1 + R2
+		(IncPc, CondAlways, AluCopy, 0, 0, 0, '1', One),   -- R0 <- 1
+		(JmpAbs, CondNotZero, AluNone, 0, 0, 0, '0', One), -- JMP 1
+		(JmpAbs, CondZero, AluNone, 0, 0, 0, '0', Zero), -- JMP 0
+		others => (JmpAbs, CondAlways, AluNone, 0, 0, 0, '0', Zero)
 		);
 
 begin
 	process(clkin)
 		variable uop : ucode_t;
+		variable res : unsigned(16 downto 0);
 	begin
 		if rising_edge(clkin) then
 			if count = 0 then
@@ -87,13 +101,32 @@ begin
 						R(uop.dst) <= uop.immediate;
 					end if;
 				elsif uop.alu_op = AluAdd then
-					R(uop.dst) <= R(uop.src_0) + R(uop.src_1);
+					res := ("0" & R(uop.src_0)) + ("0" & R(uop.src_1));
+					R(uop.dst) <= res(15 downto 0);
+					flag_C <= res(16);
+					if res(15 downto 0) = Zero then
+						flag_Z <= '1';
+					else
+						flag_Z <= '0';
+					end if;
 				end if;
 
 				if uop.pc = IncPc then
 					uPC <= uPC + 1;
 				elsif uop.pc = JmpAbs then
-					uPC <= to_integer(uop.immediate);
+					if uop.cond = CondAlways then
+						uPC <= to_integer(uop.immediate);
+					elsif uop.cond = CondZero and flag_Z = '1' then
+						uPC <= to_integer(uop.immediate);
+					elsif uop.cond = CondCarry and flag_C = '1' then
+						uPC <= to_integer(uop.immediate);
+					elsif uop.cond = CondNotZero and flag_Z = '0' then
+						uPC <= to_integer(uop.immediate);
+					elsif uop.cond = CondNotCarry and flag_C = '0' then
+						uPC <= to_integer(uop.immediate);
+					else
+						uPC <= uPC + 1;
+					end if;
 				end if;
 
 				
